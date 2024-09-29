ea1f1333fad8ed1c782f16385fdbed801bbe36e6
adenlall <adenlall@skiff.com>
2024-09-29 17:47:06 +0100

    fixes

--- resources/views/components/coupon-modal.blade.php
+++ resources/views/components/coupon-modal.blade.php
@@ -1,7 +1,7 @@
 <dialog id="coupon_modal_{{$id}}_o" class="modal">
     <div class="modal-box rounded-md shadow-lg shadow-black/30">
     <div class="text-center p-4 max-w-sm mx-auto">
-        <img class="m-auto mb-6 h-12" src="{{$store->thumbnail}}" alt="شعار متجر {{$store->_store_name}}"/>
+        <img class="m-auto mb-6 w-[135px] h-[68px]" src="{{$store->thumbnail}}" alt="شعار متجر {{$store->_store_name}}"/>
         <p class="text-xl font-bold mb-6">{{$title}}</p>
         <div class="flex rounded-full justify-center items-center m-auto my-8 p-1 w-fit bg-base-200">
             <div id="coupon_modal_{{$id}}_code" class="py-2 px-4 text-xl font-extrabold mr-1 min-w-[6em]">{{$code}}</div>
