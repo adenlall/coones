3ad6b20727003f5af069bc5ef3b9e20754c52ea0
adenlall <adenlall@skiff.com>
2024-09-29 13:07:05 +0100

    fixes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
     {!! SEO::generate() !!}
+    <!-- {!! app('seotools')->generate() !!} -->
     <!-- 
         TODO:
          * add all meta tags < Description Opengraph Twitter >
