a864f089cccb16a687efc1ddce5575226bc5bcfb
adenlall <adenlall@skiff.com>
2024-09-30 12:01:30 +0100

    fixes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,14 +1,13 @@
 <x-layout>
     <div class="w-full bg-base-100 mb-6">
         <div class="ccontainer m-auto w-full">
-            <x-breadcrumbs :items="[['name'=>'الرئيسية', 'path'=>'/'],['name'=>'تواصل معنا']]"/>
-            <h1 class="text-3xl font-medium mb-4 pb-2 mt-2">تواصل معنا</h1>
+            <x-breadcrumbs :items="[['name'=>'الرئيسية', 'path'=>'/'],['name'=>'اتصل بنا']]"/>
+            <h1 class="text-3xl font-medium mb-4 pb-2 mt-2">اتصل بنا</h1>
         </div>
     </div>
     <div class="ccontainer m-auto w-full full-width">
         <div class="min-h-[50vh] bg-base-100 rounded-md shadow-md shadow-black/30">
             <article class="p-4 px-6 pb-6 prose prose-md min-w-full">
-                <h2>اتصل بنا</h2>
                 <p>
                     يسعدنا في فريق عمل كوبون على السريع تلقي استفساراتكم وملاحظاتكم عبر البريد الإلكتروني الخاص بنا.
                     <br><br>
