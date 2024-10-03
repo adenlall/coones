85c3973472ce194ad9fd6a1f57c0e3cf9e279e85
adenlall <adenlall@skiff.com>
2024-10-03 23:07:07 +0100

    fixex

--- resources/views/components/paginator.blade.php
+++ resources/views/components/paginator.blade.php
@@ -6,20 +6,32 @@
             $lastPage = $paginator->lastPage();
             $delta = 1; // Number of pages to show on each side of the current page
         @endphp
+
+        <!-- Always show the first page link -->
         <a href="{{ $paginator->url(1) }}" class="btn rounded-md {{ $currentPage == 1 ? 'btn-primary' : 'bg-base-100' }}">
             1
         </a>
-        @if($currentPage > $delta + 2)
+
+        <!-- Show ellipsis if there are more than two pages and current page is far enough from the start -->
+        @if($currentPage > $delta + 2 && $lastPage > 3)
             <button class="btn rounded-md btn-disabled text-[gray!important] bg-[white!important]">...</button>
         @endif
+
+        <!-- Display page links around the current page -->
         @foreach (range(max(2, $currentPage - $delta), min($lastPage - 1, $currentPage + $delta)) as $page)
-            <a href="{{ $paginator->url($page) }}" class="btn rounded-md {{ $currentPage == $page ? 'btn-primary' : 'bg-base-100' }}">
-                {{ $page }}
-            </a>
+            @if ($page > 1 && $page < $lastPage) <!-- Ensure we don't show first or last page in this loop -->
+                <a href="{{ $paginator->url($page) }}" class="btn rounded-md {{ $currentPage == $page ? 'btn-primary' : 'bg-base-100' }}">
+                    {{ $page }}
+                </a>
+            @endif
         @endforeach
-        @if($currentPage < $lastPage - $delta - 1)
+
+        <!-- Show ellipsis if there are more than two pages and current page is far enough from the end -->
+        @if($currentPage < $lastPage - $delta - 1 && $lastPage > 3)
             <button class="btn rounded-md btn-disabled text-[gray!important] bg-[white!important]">...</button>
         @endif
+
+        <!-- Always show the last page link if there's more than one page -->
         @if($lastPage > 1)
             <a href="{{ $paginator->url($lastPage) }}" class="btn rounded-md {{ $currentPage == $lastPage ? 'btn-primary' : 'bg-base-100' }}">
                 {{ $lastPage }}
