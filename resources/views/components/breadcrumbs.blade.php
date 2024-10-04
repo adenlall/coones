447e521aaed171143e9fc29305bcd61524eeacae
adenlall <adenlall@skiff.com>
2024-10-04 22:36:34 +0100

    fixex

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -1,15 +1,14 @@
 @isset($items)
-<nav aria-label="Breadcrumb" class="breadcrumbs text-sm p-0 pt-2 mt-3 overflow-hidden">
-  <ol itemscope itemtype="https://schema.org/BreadcrumbList">
+<nav class="breadcrumbs text-sm p-0 pt-2 mt-3 overflow-hidden">
+  <ol>
       @foreach($items as $index => $item)
           @if(isset($item['path']))
-              <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-                <a itemprop="item" aria-label="{{$item['path']}}" href="{{$item['path']}}">
-                  <span itemprop="name">
+              <li>
+                <a aria-label="{{$item['path']}}" href="{{$item['path']}}">
+                  <span>
                     {{$item['name']}}
                   </span>
                 </a>
-                <meta itemprop="position" content="{{$index+1}}" />
               </li>
           @else
             <li>{{$item['name']}}</li>
