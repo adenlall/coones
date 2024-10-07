be54075d69c9214b83170187a2075448b2c4c790
aden <adenlall@proton.me>
2024-10-07 18:26:28 +0000

    ok

--- resources/views/components/rate.blade.php
+++ resources/views/components/rate.blade.php
@@ -2,7 +2,7 @@
     <p>هل الكود يعمل؟</p>
     <div class="flex-center-row gap-2">
         <div class="h-fit m-0">
-            <button title="Submit form" onclick="postReview({couponId:'{{$id}}', storeName:'{{urlencode($store)}}', review:'1', fingerprint:'{{\Illuminate\Support\Str::random(25)}}' })" type="submit" class="flex-center-row gap-1 border-gray-400 border-[1px] text-base-content/80 py-1 px-3 rounded">
+            <button title="Submit form" onclick="postReview({couponId:'{{$id}}', storeName:'{{urldecode($store)}}', review:'1', fingerprint:'{{\Illuminate\Support\Str::random(25)}}' })" type="submit" class="flex-center-row gap-1 border-gray-400 border-[1px] text-base-content/80 py-1 px-3 rounded">
                 <x-bx-like class="h-auto w-4"/>
                 <span>
                     نعم
@@ -10,7 +10,7 @@
             </button>
         </div>
         <div class="h-fit m-0">
-            <button title="Submit form" onclick="postReview({couponId:'{{$id}}', storeName:'{{urlencode($store)}}', review:'0', fingerprint:'{{\Illuminate\Support\Str::random(25)}}' })" type="submit" class="flex-center-row gap-1 border-gray-400 border-[1px] text-base-content/80 py-1 px-3 rounded">
+            <button title="Submit form" onclick="postReview({couponId:'{{$id}}', storeName:'{{urldecode($store)}}', review:'0', fingerprint:'{{\Illuminate\Support\Str::random(25)}}' })" type="submit" class="flex-center-row gap-1 border-gray-400 border-[1px] text-base-content/80 py-1 px-3 rounded">
                 <x-bx-dislike class="h-auto w-4"/>
                 <span>
                     لا
@@ -20,9 +20,9 @@
     </div>
 </div>
 
-<span style="display:none;" id="{{$id}}-{{urlencode($store)}}-0" class="m-auto px-3 rounded-md mt-3 py-1 bg-error text-error-content">
+<span style="display:none;" id="{{$id}}-{{urldecode($store)}}-0" class="m-auto px-3 rounded-md mt-3 py-1 bg-error text-error-content">
     متأسفين على ذلك وسوف يتم مراجعة الكوبون
 </span>    
-<span style="display:none;" id="{{$id}}-{{urlencode($store)}}-1" class="m-auto px-3 rounded-md mt-3 py-1 bg-success text-success-content">
+<span style="display:none;" id="{{$id}}-{{urldecode($store)}}-1" class="m-auto px-3 rounded-md mt-3 py-1 bg-success text-success-content">
     شكراً لك على التأكيد 🌹
 </span>
