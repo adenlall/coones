6a5a3091e6ed870cba6777f687bf8f144381e424
aden <adenlall@proton.me>
2024-10-07 18:24:56 +0000

    fixes

--- resources/views/components/rate.blade.php
+++ resources/views/components/rate.blade.php
@@ -2,7 +2,7 @@
     <p>هل الكود يعمل؟</p>
     <div class="flex-center-row gap-2">
         <div class="h-fit m-0">
-            <button title="Submit form" onclick="postReview({couponId:'{{$id}}', storeName:'{{$store}}', review:'1', fingerprint:'{{\Illuminate\Support\Str::random(25)}}' })" type="submit" class="flex-center-row gap-1 border-gray-400 border-[1px] text-base-content/80 py-1 px-3 rounded">
+            <button title="Submit form" onclick="postReview({couponId:'{{$id}}', storeName:'{{urlencode($store)}}', review:'1', fingerprint:'{{\Illuminate\Support\Str::random(25)}}' })" type="submit" class="flex-center-row gap-1 border-gray-400 border-[1px] text-base-content/80 py-1 px-3 rounded">
                 <x-bx-like class="h-auto w-4"/>
                 <span>
                     نعم
@@ -10,7 +10,7 @@
             </button>
         </div>
         <div class="h-fit m-0">
-            <button title="Submit form" onclick="postReview({couponId:'{{$id}}', storeName:'{{$store}}', review:'0', fingerprint:'{{\Illuminate\Support\Str::random(25)}}' })" type="submit" class="flex-center-row gap-1 border-gray-400 border-[1px] text-base-content/80 py-1 px-3 rounded">
+            <button title="Submit form" onclick="postReview({couponId:'{{$id}}', storeName:'{{urlencode($store)}}', review:'0', fingerprint:'{{\Illuminate\Support\Str::random(25)}}' })" type="submit" class="flex-center-row gap-1 border-gray-400 border-[1px] text-base-content/80 py-1 px-3 rounded">
                 <x-bx-dislike class="h-auto w-4"/>
                 <span>
                     لا
