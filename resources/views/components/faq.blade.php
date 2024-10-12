d108d367e6db6c87e99887f88b95a4ca43ddb08b
aden <adenlall@proton.me>
2024-10-12 13:29:48 +0000

    fixes

--- resources/views/components/faq.blade.php
+++ resources/views/components/faq.blade.php
@@ -4,8 +4,8 @@
         class="bg-base-100 collapse-plus hover:bg-accent hover:text-accent-content text-base-content focus:bg-accent focus:text-accent-content collapse rounded-sm"
     >
         <input aria-label="فتح القائمة الجانبية" name="faqcheck" type="checkbox" />
-        <div itemprop="name" class="collapse-title after:text-3xl border-gray-500 border-b-2">{{$faq->_faq_question}}</div>
-        <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer" class="collapse-content bg-primary text-primary-content">
+        <div itemprop="name" class="collapse-title after:text-3xl border-gray-500 font-bold border-b-2">{{$faq->_faq_question}}</div>
+        <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer" class="collapse-content bg-primary font-bold text-primary-content">
             <p itemprop="text" class="pt-4">{{$faq->_faq_answer}}</p>
         </div>
     </div>
