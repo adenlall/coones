4bc0a611ae8a9bb4f4e250efd055ff6852fbdb2b
aden <adenlall@proton.me>
2024-10-12 16:05:43 +0000

    flex

--- resources/views/components/cobone-card.blade.php
+++ resources/views/components/cobone-card.blade.php
@@ -2,10 +2,10 @@
     <div class="flex flex-col justify-between items-center p-3 space-y-2 w-full h-full">
         <div class="w-full space-y-2">
             <div class="flex flex-row items-center justify-between w-full">
-                <meta content="متجر {{$store->_store_name}}" />
+                <meta content="متجر {{$store->_store_name??$store->_store_param}}" />
                 <meta content="{{'https://coupon3sari3.com/coupons/'.$store->_store_name}}" />
                 <a aria-label="زيارة العرض" href="{{'/coupons/'.($store->_store_param===null?$store->_store_name:$store->_store_param)}}">
-                    <img class="w-[135px] h-[68px] rounded-md" alt="شعار متجر {{$store->_store_name}}" src="{{$store->thumbnail}}"/>
+                    <img class="w-[135px] h-[68px] rounded-md" alt="شعار متجر {{$store->_store_name??$store->_store_param}}" src="{{$store->thumbnail}}"/>
                 </a>
                 <div class="flex-center-row gap-2" onclick="{{'share_modal_'.$id.'_o'}}.showModal()" >
                     <x-tabler-share class="hover:bg-base-200/80 rounded-full h-8 w-8 p-1 cursor-pointer" />
@@ -18,7 +18,7 @@
         <h3 class="text-xl text-right w-full h-full">{{$title}}</h3>
         <hr class="border-base-content/30 w-full"/>
         <div class="w-[100%] space-y-2">
-            <a aria-label="{{$store->_store_name}}" href="/coupons/{{$store->_store_param===null?$store->_store_name:$store->_store_param}}" class="btn rounded-sm btn-sm btn-ghost font-bold text-lg w-full mt-3">عرض جميع الكوبونات</a>
+            <a aria-label="{{$store->_store_name??$store->_store_param}}" href="/coupons/{{$store->_store_param===null?$store->_store_name:$store->_store_param}}" class="btn rounded-sm btn-sm btn-ghost font-bold text-lg w-full mt-3">عرض جميع الكوبونات</a>
             <div class="w-full" dir="ltr">
                 @if($type==="3" && isset($link))
                     <a aria-label="الحصول على العرض" href="{{$link}}" target="_blank" class="btn btn-accent h-[3rem] font-bold text-xl rounded-md min-h-min w-full">
